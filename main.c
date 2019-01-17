d507b4be9aba24b17591d6b6d8cfc4b440a7117c
jkjnmlk <123@example.com>
2019-01-17 11:42:56 +0800

    增加车辆位置功能

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
-int main(){
+struct point
+{
+    int x;
+    int y;
+};
 
-    printf("欢迎进入专场管理系统\n");
-    
-    while(1){
+int main()
+{
+    struct point arr[100];
+    int index = 0;
+
+    printf("欢迎进入专车管理系统\n");
+
+    while (1)
+    {
         printf("1--添加车辆位置\n");
         printf("2--删除最后一个车辆位置\n");
         printf("3--显示所有车辆位置\n");
@@ -12,29 +22,41 @@ int main(){
         printf("5--退出\n");
         printf("请选择相应功能\n");
         int code;
-        scanf("%d",&code);
-        
-        if (code==1) {
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+            printf("请输入车辆位置x坐标与y坐标\n");
+            int x;
+            int y;
+            scanf("%d", &x);
+            scanf("%d", &y);
+            arr[index].x=x;
+            arr[index].y=y;
+            index++;
+            printf("添加成功，点击回车继续\n");
+            char a;
+            scanf("%c",&a);
+            scanf("%c",&a);
+        }
+        if (code == 2)
+        {
             /* code */
         }
-         if (code==2) {
+        if (code == 3)
+        {
             /* code */
         }
-         if (code==3) {
+        if (code == 4)
+        {
             /* code */
         }
-         if (code==4) {
-            /* code */
-        }
-         if (code==5) {
+        if (code == 5)
+        {
             printf("点击回车，退出系统\n");
             break;
         }
-        
-        
     }
-    
 
     return 0;
-
 }
